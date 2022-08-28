4564e87b50cfd91813a51e91ccec7f69b2de746d
darshan51081@gmail.com <darshan51081@gmail.com>
2022-08-29 01:56:48 +0530

    My First Commit

---
+++ lib/article_display_screen.dart
@@ -0,0 +1,191 @@
+library article_display_screen;
+
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class CustomTag extends StatelessWidget {
+  const CustomTag(
+      {Key? key, required this.backgroundColor, required this.children})
+      : super(key: key);
+
+  final Color backgroundColor;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      child: Row(
+        children: children,
+      ),
+    );
+  }
+}
+
+class ArticleDisplayScreen extends StatelessWidget {
+  const ArticleDisplayScreen ({
+    Key? key,
+    this.height,
+    this.borderRadius = 20,
+    required this.width,
+    this.imageUrl,
+    this.padding,
+    this.margin,
+    this.child,
+    this.subtitle,
+    this.title,
+    this.author,
+    this.content,
+    this.date,
+  }) : super(key: key);
+  final double width;
+  final height;
+  final imageUrl;
+  final subtitle;
+  final title;
+  final author;
+  final content;
+  final date;
+
+  final EdgeInsets? padding;
+  final EdgeInsets? margin;
+  final double borderRadius;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: height,
+      width: double.infinity,
+      margin: margin,
+      padding: padding,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        image: DecorationImage(
+          image: NetworkImage(imageUrl),
+          fit: BoxFit.cover,
+        ), // DecorationImage
+      ),
+      // BoxDecoration
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          iconTheme: const IconThemeData(
+            color: Colors.white,
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+        ),
+        extendBodyBehindAppBar: true,
+        body: ListView(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // SizedBox(height: MediaQuery.of(context).size.aspectRatio = 0.15,),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      height: 1.25,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    subtitle, // TODO: add subtitle
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(20.0),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(
+                    20,
+                  ),
+                  topRight: Radius.circular(
+                    20,
+                  ),
+                ),
+              ),
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      CustomTag(
+                        backgroundColor: Colors.black38,
+                        children: [
+                          Text(
+                            author,
+                            style:
+                            Theme.of(context).textTheme.bodySmall!.copyWith(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      CustomTag(
+                        backgroundColor: Colors.black38,
+                        children: [
+                          Text(
+                            date,
+                            style:
+                            Theme.of(context).textTheme.bodySmall!.copyWith(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10.0,
+                  ),
+                  Text(
+                    title,
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    content,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Colors.black,
+                      height: 1.5,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
